9e70cc9a4383cbef56a6a100cdf77e963922f8f2
MariyaPikalova <it.pikalova@gmail.com>
2019-05-21 16:36:23 +0300

    add typification with flow

--- src/reducers/buttonMore.js
+++ src/reducers/buttonMore.js
@@ -1,10 +1,16 @@
+//@flow
 import { SHOW_BUTTON_MORE } from "../actions/PageActions";
+
+
 
 const initialState = {
   buttonMore: false
 };
+type State = {+buttonMore: boolean}
+type Action = {type: "SHOW_BUTTON_MORE"}
 
-const buttonMore = (state = initialState, action) => {
+
+const buttonMore = (state: State = initialState, action: Action) => {
   switch (action.type) {
     case SHOW_BUTTON_MORE:
       return {
